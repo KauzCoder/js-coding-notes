29eccbdd4fed312276229c86338bb2cb1cc27e61
KauzCoder <kauzcoder.dev@gmail.com>
2025-12-18 02:30:26 -0300

    feat(timer): atualiza botão, exibe e formata cronômetro por contexto
    Implementa a lógica para alternar dinamicamente o texto e o ícone
    do botão do temporizador entre "Começar" (play) e "Pausar" (pause),
    de acordo com o estado do cronômetro.
    Exibe o tempo diretamente na tela, na div #timer, utilizando
    innerHTML e o objeto Date do JavaScript.
    Configura o formato do tempo para o padrão "00:00", com minutos
    e segundos corretamente formatados.
    Define tempos específicos para cada contexto do temporizador:
    - Foco: 1500 segundos
    - Descanso curto: 300 segundos
    - Descanso longo: 900 segundos

--- projetos/Fokus-projeto-base/Fokus-projeto-base/script.js
+++ projetos/Fokus-projeto-base/Fokus-projeto-base/script.js
@@ -11,8 +11,11 @@ const musica = new Audio('/sons/luna-rise-part-one.mp3');
 const soundPlay = new Audio('/sons/play.wav');
 const soundPause = new Audio('/sons/pause.mp3');
 const soundStop = new Audio('/sons/beep.mp3');
+const textoBtnIniciarOuPausar = document.querySelector('#start-pause span');
+const iconeBtnIniciarOuPausar = document.querySelector('.app__card-primary-butto-icon');
+const tempoDisplay = document.querySelector('#timer');
 
-let tempoDecorridoEmSegundos = 15000;
+let tempoDecorridoEmSegundos;
 let intervaloId = null
 
 musicaFocoInput.addEventListener('change', () => {
@@ -24,24 +27,26 @@ musicaFocoInput.addEventListener('change', () => {
   }
 });
 
-
-
 focoBtn.addEventListener('click', () => {
+  tempoDecorridoEmSegundos = 25 * 60
   alterarContexto('foco')
   focoBtn.classList.add('active')
 });
 
 curtoBtn.addEventListener('click', () => {
+  tempoDecorridoEmSegundos = 5 * 60
   alterarContexto('descanso-curto')
   curtoBtn.classList.add('active')
 });
 
 longoBtn.addEventListener('click', () => {
+  tempoDecorridoEmSegundos = 15 * 60
   alterarContexto('descanso-longo')
   longoBtn.classList.add('active')
 });
 
-alterarContexto = (contexto) => {
+const alterarContexto = (contexto) => {
+  mostrarTempo();
   html.setAttribute('data-contexto', contexto)
   banner.setAttribute('src', `/imagens/${contexto}.png`)
   botoes.forEach((contexto) => contexto.classList.remove('active'))
@@ -49,12 +54,15 @@ alterarContexto = (contexto) => {
     case 'foco':
       titulo.innerHTML = `Otimize sua produtividade,<br>
                 <strong class="app__title-strong">mergulhe no que importa.</strong>`
+      encerrar()
       break;
-    case 'descanso-curto':
+      case 'descanso-curto':
       titulo.innerHTML = `Que tal dar uma respirada?  <strong class="app__title-strong">Faça uma pausa curta!</strong>`
+      encerrar()
       break;
     case 'descanso-longo':
       titulo.innerHTML = `Hora de voltar à superfície.  <strong class="app__title-strong">Faça uma pausa longa.</strong>`
+      encerrar()
       break;
   }
 }
@@ -63,12 +71,12 @@ alterarContexto = (contexto) => {
 const contagemRegressiva = () => {
   if (tempoDecorridoEmSegundos <= 0) {
     soundStop.play()
-    encerrar()
     alert('Tempo finalizado!')
+    encerrar()
     return
   }
   tempoDecorridoEmSegundos -= 1
-  console.log(tempoDecorridoEmSegundos)
+  mostrarTempo();
 }
 
 // Função de iniciar/pausar (executada no click)
@@ -80,6 +88,8 @@ function iniciarOuPausar() {
   }
   soundPlay.play();
   intervaloId = setInterval(contagemRegressiva, 1000)
+  textoBtnIniciarOuPausar.textContent = 'Pausar'
+  iconeBtnIniciarOuPausar.setAttribute('src',`/imagens/pause.png`)
 }
 
 // Listener correto
@@ -88,5 +98,14 @@ startPauseBtn.addEventListener('click', iniciarOuPausar)
 function encerrar() {
   clearInterval(intervaloId)
   intervaloId = null
+  textoBtnIniciarOuPausar.textContent = 'Começar'
+  iconeBtnIniciarOuPausar.setAttribute('src',`/imagens/play_arrow.png`)
 }
 
+function mostrarTempo() {
+  const tempo = new Date(tempoDecorridoEmSegundos * 1000);
+  const tempoFormatado = tempo.toLocaleTimeString('pt-Br', {minute: '2-digit', second: '2-digit'});
+  tempoDisplay.innerHTML = `${tempoFormatado}`;
+}
+
+mostrarTempo(); 
