218840bc723dca54d030910eb82fde55ac168914
KauzCoder <kauzcoder.dev@gmail.com>
2025-12-18 01:34:35 -0300

    feat(timer): implementa contagem regressiva, pausa e controle com áudio
    Adiciona a contagem regressiva automática do temporizador
    utilizando setInterval(), exibindo o tempo no console.
    Refatora o código para permitir pausar a contagem por meio
    do clique no botão, usando clearInterval().
    Integra efeitos sonoros para iniciar, pausar o temporizador
    e sinalizar quando o tempo chega ao fim, tornando a
    experiência do usuário mais interativa.

--- projetos/Fokus-projeto-base/Fokus-projeto-base/script.js
+++ projetos/Fokus-projeto-base/Fokus-projeto-base/script.js
@@ -4,22 +4,47 @@ const curtoBtn = document.querySelector('.app__card-button--curto');
 const longoBtn = document.querySelector('.app__card-button--longo');
 const banner = document.querySelector('.app__image');
 const titulo = document.querySelector('.app__title');
+const botoes = document.querySelectorAll('.app__card-button');
+const startPauseBtn = document.querySelector('#start-pause');
+const musicaFocoInput = document.querySelector('#alternar-musica');
+const musica = new Audio('/sons/luna-rise-part-one.mp3');
+const soundPlay = new Audio('/sons/play.wav');
+const soundPause = new Audio('/sons/pause.mp3');
+const soundStop = new Audio('/sons/beep.mp3');
+
+let tempoDecorridoEmSegundos = 15000;
+let intervaloId = null
+
+musicaFocoInput.addEventListener('change', () => {
+  if (musica.paused) {
+    musica.play();
+    musica.loop = true;
+  } else {
+    musica.pause();
+  }
+});
+
+
 
 focoBtn.addEventListener('click', () => {
   alterarContexto('foco')
+  focoBtn.classList.add('active')
 });
 
 curtoBtn.addEventListener('click', () => {
-alterarContexto('descanso-curto')
+  alterarContexto('descanso-curto')
+  curtoBtn.classList.add('active')
 });
 
 longoBtn.addEventListener('click', () => {
   alterarContexto('descanso-longo')
+  longoBtn.classList.add('active')
 });
 
 alterarContexto = (contexto) => {
   html.setAttribute('data-contexto', contexto)
   banner.setAttribute('src', `/imagens/${contexto}.png`)
+  botoes.forEach((contexto) => contexto.classList.remove('active'))
   switch (contexto) {
     case 'foco':
       titulo.innerHTML = `Otimize sua produtividade,<br>
@@ -33,3 +58,35 @@ alterarContexto = (contexto) => {
       break;
   }
 }
+
+// Função que decrementa o tempo (executada pelo intervalo)
+const contagemRegressiva = () => {
+  if (tempoDecorridoEmSegundos <= 0) {
+    soundStop.play()
+    encerrar()
+    alert('Tempo finalizado!')
+    return
+  }
+  tempoDecorridoEmSegundos -= 1
+  console.log(tempoDecorridoEmSegundos)
+}
+
+// Função de iniciar/pausar (executada no click)
+function iniciarOuPausar() {
+  if(intervaloId){
+    soundPause.play();
+    encerrar()
+    return
+  }
+  soundPlay.play();
+  intervaloId = setInterval(contagemRegressiva, 1000)
+}
+
+// Listener correto
+startPauseBtn.addEventListener('click', iniciarOuPausar)
+
+function encerrar() {
+  clearInterval(intervaloId)
+  intervaloId = null
+}
+
